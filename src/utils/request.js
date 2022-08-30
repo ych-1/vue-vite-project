6682a3f112a02e2e7c558dd2bb1a860e78e4bb48
ych-1 <930971720@qq.com>
2022-08-30 22:30:44 +0800

    first commit

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,11 +9,11 @@ const service = axios.create({
 
 // 添加请求拦截器
 service.interceptors.request.use(
-  function (config) {
+  (config) => {
     // 在发送请求之前做些什么
     return config
   },
-  function (error) {
+  (error) => {
     // 对请求错误做些什么
     return Promise.reject(error)
   }
@@ -21,12 +21,12 @@ service.interceptors.request.use(
 
 // 添加响应拦截器
 service.interceptors.response.use(
-  function (response) {
+  (response) => {
     // 2xx 范围内的状态码都会触发该函数。
     // 对响应数据做点什么
     return response
   },
-  function (error) {
+  (error) => {
     // 超出 2xx 范围的状态码都会触发该函数。
     // 对响应错误做点什么
     return Promise.reject(error)
